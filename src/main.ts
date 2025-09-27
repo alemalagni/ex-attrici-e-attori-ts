ab8688ab81d319bf07db62189f9828e5f0ed7aaa
alemalagni <alemalagni@gmail.com>
2025-09-27 18:15:12 +0200

    add Person type definition to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import './style.css'
 
+type Person = {
+  readonly id: number;
+  readonly name: string;
+  birth_year: number;
+  death_year?: number;
+  biography: string;
+  image: string;
+}
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
   </div>
 `
+
